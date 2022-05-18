a18a9708838401157def18111a6ba8a18446694a
BrunoSouzax <bruno_sdsouza@hotmail.com>
2022-05-18 12:21:05 -0300

    conteudo

---
+++ react/aula01/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./styles.css"
+
+const element = 'Bruno Silva'
+const element2 = <h1> Hello World =D </h1>
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(element2, rootElement)
